88a62014ef0e8715df91bc6a218e14f411f94918
Mah-irajpoot <mahi9242doll@gmail.com>
2025-07-18 18:57:33 +0500

    Create mahi05.py
    rm -rf CYBER 
    git clone --depth=1 https://github.com/Mah-irajpoot/Mahirajpoot05.git/MAHI-TOOL/CYBER
    cd CYBER 
    chmod 777 MAHI
    ./RAJPOOT

---
+++ mahi05.py
@@ -0,0 +1,3 @@
+datr=_SRxaChECl_IT0EGlVcNDMaG;fr=005IpSvYWWbfbjQs5.AWd0  mDQmKFRXkhVxNTVFZn32_LegYx0THvr1Qem3GswLyA-fLrA.BocST9.  .AAA.0.0.BocSVE.AWebfnZKnlMlXdwhRdSy9kFxKSk;sb=_iRxaLrk  CYNAwH-twRytnC3z;c_user=61578239430874;xs=34%3ABwwoIDoh  qzVhEA%3A2%3A1752245579%3A-1%3A-1                         USERAGENT : Mozilla/5.0 (Linux; Ubuntu/20.03; Android  
+ 10; K) AppleWebKit/537.36 (KHTML, like Gecko)           
+ Chrome/108.0.6494.223 Safari/537.36                      GoogleApp/11.35.63.25.arm55
